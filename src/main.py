e56319620d4a64ed672e51588a052c26df52d91b
marbou090 <rikka2217@gmail.com>
2022-02-08 06:27:18 +0900

    node state

--- src/main.py
+++ src/main.py
@@ -1,9 +1,31 @@
 import pyxel
 
+MOUSE_KEY=0
+SELECT_KEY=0 #0で通常、1で選択
+NODE_STATE=0 #0で通常、1で満タン
+
+
+class Bridge:
+    def __init__(self):
+        self.node= 0 #通常ノードは0,0に格納されてるタイルなので0
+    
+    def node_state(self,state_key):
+        if state_key==1:
+            #選択されたら選択ノードを出すので0,32に入っているタイルを返したい
+            self.node=32
+        else:
+            self.node=0
+        return self.node
+        
+        
+
 class App:
     def __init__(self):
         pyxel.init(200, 200,title="BridgePuzzle")
+        pyxel.mouse(True)
         pyxel.load("bridgepicture.pyxres")
+        self.bridge=Bridge()
+        self.select_key=0
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -12,9 +34,13 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
-            pyxel.blt(0, 0, 0, 0, 16, 16, 16)
-        else:
-            pyxel.blt(0, 0, 0, 0, 0, 16, 16)
+            
+        #クリックしたら選択ノードになって、もう一度クリックで戻す
+        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT) and self.select_key==0:
+            self.select_key=1
+        elif pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT) and self.select_key==1:
+            self.select_key=0
+        
+        pyxel.blt(0, 0, 0, 0, self.bridge.node_state(self.select_key), 16, 16)
 
 App()
